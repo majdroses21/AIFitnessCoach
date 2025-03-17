3920ba75cdf20cb9edbac6ef3c587211aee9c4b4
Majd Ahmed <majdrose@gmail.com>
2025-03-17 16:59:21 +0300

    Add GYM Layout

---
+++ components/GymFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-footer
+        class="bg-grey-darken-4 text-center d-flex flex-column" dark
+      >
+        <div>
+          <v-btn
+            v-for="icon in ['mdi-facebook', 'mdi-twitter', 'mdi-instagram']"
+            :key="icon"
+            class="mx-4"
+            :icon="icon"
+            variant="text"
+          ></v-btn>
+        </div>
+  
+        <div class="pt-4">
+          <v-btn
+            v-for="link in ['health system','page2', 'Page3']"
+            :key="link"
+            variant="text"
+            class="mx-3"
+            color="white"
+          >
+            {{ link }}
+          </v-btn>
+        </div>
+  
+        <v-divider class="my-4"></v-divider>
+  
+        <div class="text-white">
+          {{ new Date().getFullYear() }} — <strong>AI Fitness Coach</strong>
+        </div>
+      </v-footer>
+</template>
