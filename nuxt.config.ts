55b6e7a671b2561d666903ee8de799b65e0eb22c
AMAJ11 <apo.zouher@gmail.com>
2025-03-17 16:16:11 +0300

    Exercises&profilePage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   build: {
